d33effc504f54dc9184ae4446f624d3e703bc3a2
fanhaoyuan <418536538@qq.com>
2024-03-29 22:26:08 +0800

    feat: 添加电子邮箱

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,24 @@
+import { AppLanguage, useAppContext } from '../../contexts/useAppContext';
 import { LanguageButton } from './LanguageButton';
 
 export function Footer() {
+  const language = useAppContext(x => x.language);
+
+  const emailMap = {
+    [AppLanguage.EN_US]: 'E-mail :purchase@liminikids.com',
+    [AppLanguage.ZH_CN]: '电子邮箱 :purchase@liminikids.com',
+  };
+
   return (
     <div className='bg-[#121212] w-full h-[220px] px-[80px]'>
       <div className='w-full h-full max-w-[1440px] flex items-center mx-auto'>
-        <div className='text-white font-bold text-[48px]'>LIMINI®</div>
+        <div>
+          <div className='text-white font-bold text-[48px]'>
+            LIMINI®
+            <div className='text-white font-medium text-[20px]'>{emailMap[language]}</div>
+          </div>
+        </div>
+
         <div className='ml-auto text-white'>
           <LanguageButton />
         </div>
